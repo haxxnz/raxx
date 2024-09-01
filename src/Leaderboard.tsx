45e2da5f604bdd284c85b03e9ca58eb2f6724ee2
Russell Bloxwich <me@reb.nz>
2024-09-01 15:12:51 +1200

    ship leaderboard

--- src/Leaderboard.tsx
+++ src/Leaderboard.tsx
@@ -1,40 +1,116 @@
-import React, { useEffect, useState } from "react";
-import CrimeGroupsList from "./components/CrimeGroupsList";
+import { useEffect, useState } from "react";
 import CrimeSummary from "./components/CrimeSummary";
+import { CrimeData } from "./types";
 
-interface CrimeData {
-  [key: string]: unknown; // Replace with a more specific type if possible
-}
+const totalUrls = {
+  "2024":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboards/2024.json",
+  "2023":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboards/2023.json",
+  "2022":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboards/2022.json",
+  "2021":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboards/2021.json",
+  "2020":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboards/2020.json",
+};
+
+const divisionUrls = {
+  "Abduction harassment and other related offences against a person":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_divisions/abduction_harassment_and_other_related_offences_against_a_person.json",
+  "Acts intended to cause injury":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_divisions/acts_intended_to_cause_injury.json",
+  "Robbery extortion and related offences":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_divisions/robbery_extortion_and_related_offences.json",
+  "Sexual assault and related offences":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_divisions/sexual_assault_and_related_offences.json",
+  "Theft and related offences":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_divisions/theft_and_related_offences.json",
+  "Unlawful entry with intentburglary break and enter":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_divisions/unlawful_entry_with_intentburglary_break_and_enter.json",
+};
+
+const subdivisionUrls = {
+  "Abduction and kidnapping":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/abduction_and_kidnapping.json",
+  "Aggravated robbery":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/aggravated_robbery.json",
+  "Aggravated sexual assault":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/aggravated_sexual_assault.json",
+  "Blackmail and extortion":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/blackmail_and_extortion.json",
+  "Common assault":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/common_assault.json",
+  "Illegal use of a motor vehicle":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/illegal_use_of_a_motor_vehicle.json",
+  "Illegal use of property except motor vehicles":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/illegal_use_of_property_except_motor_vehicles.json",
+  "Nonaggravated robbery":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/nonaggravated_robbery.json",
+  "Nonaggravated sexual assault":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/nonaggravated_sexual_assault.json",
+  "Serious assault not resulting in injury":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/serious_assault_not_resulting_in_injury.json",
+  "Serious assault resulting in injury":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/serious_assault_resulting_in_injury.json",
+  "Theft except motor vehicles nec":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/theft_except_motor_vehicles_nec.json",
+  "Theft from a person excluding by force":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/theft_from_a_person_excluding_by_force.json",
+  "Theft from retail premises":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/theft_from_retail_premises.json",
+  "Theft of a motor vehicle":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/theft_of_a_motor_vehicle.json",
+  "Theft of motor vehicle parts or contents":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/theft_of_motor_vehicle_parts_or_contents.json",
+  "Unlawful entry with intentburglary break and enter":
+    "https://raw.githubusercontent.com/haxxnz/raxx-data/main/data/leaderboard_2024_subdivisions/unlawful_entry_with_intentburglary_break_and_enter.json",
+};
+
+const urls = {
+  Total: "placeholder", // this is a lazy hack
+  ...divisionUrls,
+  ...subdivisionUrls,
+};
+
+type UrlsMap = typeof urls;
+
+const getUrl = (selectedCrime: keyof UrlsMap, filterTime: string) => {
+  if (selectedCrime === "Total") {
+    return totalUrls[filterTime as keyof typeof totalUrls];
+  }
+  return urls[selectedCrime];
+};
+
+const getLeadData = async (
+  selectedCrime: keyof UrlsMap,
+  filterTime: string
+) => {
+  const url = getUrl(selectedCrime, filterTime);
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error("Failed to fetch crime data:", error);
+    return null;
+  }
+};
 
 function Leaderboard() {
   const [crimeData, setCrimeData] = useState<CrimeData | null>(null);
-  const [crimeNames, setCrimeNames] = useState<string[]>([]);
-  const [selectedCrime, setSelectedCrime] = useState("all");
-  const [timeFilter, setTimeFilter] = useState("year");
-
-  async function getLeadData() {
-    const url =
-      "https://raw.githubusercontent.com/haxxnz/raxx-data/main/crime_by_year_20240825.json";
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-      return await response.json();
-    } catch (error) {
-      console.error("Failed to fetch crime data:", error);
-      return null;
-    }
-  }
+  const [selectedCrime, setSelectedCrime] = useState<keyof UrlsMap>("Total");
+  const [timeFilter, setTimeFilter] = useState("2024");
 
   useEffect(() => {
-    getLeadData().then((data) => {
+    getLeadData(selectedCrime, timeFilter).then((data) => {
       if (data) {
         setCrimeData(data);
-        setCrimeNames(Object.keys(data));
       }
     });
-  }, []);
+  }, [selectedCrime, timeFilter]);
 
   return (
     <>
@@ -50,15 +126,29 @@ function Leaderboard() {
             <select
               id="category"
               value={selectedCrime}
-              onChange={(e) => setSelectedCrime(e.target.value)}
+              onChange={(e) => {
+                setSelectedCrime(e.target.value as keyof UrlsMap);
+                if (e.target.value !== "Total") {
+                  setTimeFilter("2024");
+                }
+              }}
               className="p-2 rounded-md mt-2 w-full text-white"
             >
-              <option value="all">All Crime</option>
-              {crimeNames.map((name) => (
-                <option key={name} value={name}>
-                  {name}
-                </option>
-              ))}
+              <option value={"Total"}>Total</option>
+              <optgroup label="Divisions">
+                {Object.keys(divisionUrls).map((name) => (
+                  <option key={name} value={name}>
+                    {name}
+                  </option>
+                ))}
+              </optgroup>
+              <optgroup label="Subdivisions">
+                {Object.keys(subdivisionUrls).map((name) => (
+                  <option key={name} value={name}>
+                    {name}
+                  </option>
+                ))}
+              </optgroup>
             </select>
           </div>
           <div>
@@ -68,30 +158,29 @@ function Leaderboard() {
             <select
               id="time"
               value={timeFilter}
+              disabled={selectedCrime !== "Total"}
               onChange={(e) => setTimeFilter(e.target.value)}
               className="p-2 rounded-md mt-2 w-full text-white"
             >
-              <option value="year">This year</option>
-              <option value="month">This Month</option>
+              <option value="2024">2024</option>
+              <option value="2023">2023</option>
+              <option value="2022">2022</option>
+              <option value="2021">2021</option>
+              <option value="2020">2020</option>
             </select>
           </div>
         </div>
       </section>
       <section className="max-lg:m-4 max-w-6xl mx-auto mt-8">
         <h1 className="text-2xl font-semibold">
-          {selectedCrime === "all" ? "All Incidents" : selectedCrime} /{" "}
-          {timeFilter === "year" ? "This year" : "This month"}
+          {selectedCrime} /{" "}
+          {timeFilter === new Date().getFullYear().toString()
+            ? "This year"
+            : timeFilter}
         </h1>
       </section>
       <section className="max-lg:m-4 max-w-6xl mx-auto mt-8 border-gray-700 border rounded-xl p-6 bg-gray-950">
-        <div className="flex justify-between font-semibold mb-8">
-          <p className="flex-1 text-lg">Location</p>
-          <p className="flex-1 text-lg text-end">Frequency</p>
-        </div>
-
-        {crimeData && (
-          <CrimeSummary crimeData={crimeData[selectedCrime] || crimeData} />
-        )}
+        {crimeData && <CrimeSummary crimeData={crimeData} />}
       </section>
     </>
   );
--- src/components/CrimeSummary.tsx
+++ src/components/CrimeSummary.tsx
@@ -1,42 +1,19 @@
 import React, { useMemo } from "react";
+import { CrimeData } from "../types";
 
-const CrimeSummary = ({ crimeData }) => {
-  const currentYear = new Date().getFullYear().toString();
-
-  const processedData = useMemo(() => {
-    const result = {};
-
-    Object.entries(crimeData).forEach(([crimeType, locations]) => {
-      result[crimeType] = Object.entries(locations)
-        .map(([location, yearData]) => ({
-          location,
-          incidents: yearData[currentYear] || 0,
-        }))
-        .filter((item) => item.incidents > 0) // Only include locations with incidents in the current year
-        .sort((a, b) => b.incidents - a.incidents);
-    });
-
-    return result;
-  }, [crimeData, currentYear]);
-
+const CrimeSummary = ({ crimeData }: { crimeData: CrimeData }) => {
+  console.log({ crimeData });
   return (
     <div className="p-4">
-      {Object.entries(processedData).map(([crimeType, locations]) => (
-        <div key={crimeType} className="mb-6">
-          <h3 className="text-xl font-semibold mb-2">{crimeType}:</h3>
-          {locations.map((loc, index) => (
-            <p key={loc.location} className="ml-4">
-              {index + 1}. {loc.location} {loc.incidents} incidents in{" "}
-              {currentYear}
-            </p>
-          ))}
-          {locations.length === 0 && (
-            <p className="ml-4 italic">
-              No incidents reported in {currentYear}
-            </p>
-          )}
-        </div>
-      ))}
+      {crimeData.map(([location, count]) => {
+        console.log({ location, count });
+        return (
+          <div className="mb-6">
+            <p className="text-xl font-semibold mb-2">{location}:</p>
+            <p className="ml-4 italic">{count} crimes reported.</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
