15806994f5a0171436b9791699cfc254fd71ef94
Walter Lim <waltissomewhere@gmail.com>
2024-09-03 10:18:21 +1200

    aaaaa

--- src/components/CrimeSummary.tsx
+++ src/components/CrimeSummary.tsx
@@ -6,16 +6,23 @@ const CrimeSummary = ({ crimeData }: { crimeData: CrimeData }) => {
     <div className="p-4">
       <div className="mb-6 flex justify-between align-center ">
         <p className="text-lg font-semibold mb-2">Location</p>
-        <p className="ml-4 text-lg font-semibold">Crimes reported.</p>
+        <p className="ml-4 text-lg font-semibold">Crimes reported</p>
       </div>
       {crimeData.map(([location, count], index) => {
         console.log({ location, count });
         return (
           <div className="mb-2 flex justify-between align-center border-t border-gray-700 pt-4">
-            <p className="text-md  mb-2">
+            <a
+              target="_blank"
+              rel="noreferrer"
+              className="text-md  mb-2 text-blue-300 underline"
+              href={`https://www.google.com/maps/search/${encodeURIComponent(
+                location.replace(" ", "+")
+              )}`}
+            >
               {index + 1}. {location}:
-            </p>
-            <p className=" text-gray-300">{count} Incidents</p>
+            </a>
+            <p className=" text-gray-300">{count.toLocaleString()} incidents</p>
           </div>
         );
       })}
